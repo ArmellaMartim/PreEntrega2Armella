24258ac40035738a4c97b37a1f43e3bed677b6a1
Martim Armella <armellam7@gmail.com>
2023-03-02 16:24:14 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,96 @@
+function $(selector) {
+  return document.querySelector(selector);
+}
+function Carrito() {
+  this.catalogo = [
+    {
+      id: "P01",
+      nombre: "Andador",
+      precio: 3000,
+      imagen: "Andador.png",
+      descripcion:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      id: "P02",
+      nombre: "Silla de ducha",
+      precio: 5000,
+      imagen: "silla de ducha.jpg",
+      descripcion:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      id: "P03",
+      nombre: "Silla de ruedas",
+      precio: 13000,
+      imagen: "silla de rueda.jpg",
+      descripcion:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      id: "P04",
+      nombre: "Guantes",
+      precio: 1500,
+      imagen: "guantes.jpg",
+      descripcion:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      id: "P06",
+      nombre: "Venda",
+      precio: 750,
+      imagen: "venda.jpg",
+      descripcion:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      id: "P07",
+      nombre: "Nebulizador",
+      precio: 12000,
+      imagen: "nebulizador.jpg",
+      descripcion:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ];
+  this.constructor = function () {
+    if (!localStorage.getItem("carrito")) {
+      localStorage.setItem("carrito", "[]");
+    }
+  };
+  this.getCarrito = JSON.parse(localStorage.getItem("carrito"));
+}
+
+function VerCarrito() {
+  this.renderCatalogo = function () {
+    let template = ``;
+    for (let i in carrito.catalogo) {
+      template += `
+            <div class="column is-one-quarter">
+            <div class="card">
+                <div class="card-image">
+                    <img src="../img/${carrito.catalogo[i].imagen}" alt="Placeholder">
+                </div>
+                <div class="card-content">
+                    <h2 class="title is-3">${carrito.catalogo[i].nombre}</h2>
+                    <p>${carrito.catalogo[i].descripcion}</p>
+                    <br>
+                    <h3 class="subtitle is-4">Precio: <strong>$${carrito.catalogo[i].precio}</strong></h3>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="card-footer-item" id="addItem" data-producto="${carrito.catalogo[i].id}">Agregar al Carrito</a>
+                </div>
+                </div>
+            </div>
+            `;
+    }
+    $("#catalogo").innerHTML = template;
+  };
+}
+
+let carrito = new Carrito();
+let carritoVer = new VerCarrito();
+
+document.addEventListener("DOMContentLoaded", function () {
+  carritoVer.renderCatalogo();
+  carrito.constructor();
+});
